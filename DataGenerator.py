2dbf29d54da293f1b76367ca0bf877ae965b63b8
drgn0 <70508632+drgn0@users.noreply.github.com>
2022-03-31 00:10:22 +0530

    Update DataGenerator.py

--- DataGenerator.py
+++ DataGenerator.py
@@ -1,31 +1,51 @@
 import numpy as np 
 import random 
 
+
 class Line:
     def __init__(self, w, b):
         # y = w1*x1 + w2x2 + ... wn*xn  +  b 
+        try:
+            w[0]  
+        except Exception:
+            raise TypeError("w should be iterable")
+
         self.w = w 
         self.b = b 
+
+        # the 'range' of data. 
+        # y_max - y_min 
+        self.spread = abs(self.f(2) - self.f(-2))  # considering  -2 <= x < 2 
     
     def f(self, x) -> float:
         # f(x) = w1*x1 + w2x2 + ... wn*xn  +  b 
         return np.dot(self.w, x) + self.b 
 
-    def generate(self, n = 500, standard_dev = 0.4):
-        return np.array(
-            self.generate_data_point(standard_dev) for i in range(n)
-        )
+    def generate(self, n = 200, err = 0.2):
+        data = np.empty(n, tuple) 
+        generator = (self.generate_data_point(err) for i in range(n))  
+
+        for i, val in enumerate(generator):
+            data[i] = val 
         
-    def generate_data_point(self, standard_dev: float):
+        return data 
+        
+        
+    def generate_data_point(self, err: float):
+        '''returns a single data point'''
         x = np.random.random((len(self.w)))
-        x = (x - 0.5) * 5  #  x belongs to [-2.5, 2.5) now 
+        x = (x - 0.5) * 4  #  x belongs to [-2, 2) now 
 
         y = self.f(x) 
-        y = random.normalvariate(y, standard_dev)  # randomness 
+        ## y = random.normalvariate(y, standard_dev)  # randomness 
+        y += (random.random() - random.random()) * self.spread * err
 
         return x, y 
     
 if __name__ == '__main__':
-    pass 
+    line = Line((2,), 5)
+    data = line.generate() 
+
+    print(data[:5]) 
 
     
